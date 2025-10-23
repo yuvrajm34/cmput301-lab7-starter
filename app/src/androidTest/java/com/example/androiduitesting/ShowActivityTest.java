bfebb086f32c4eeca3b3726df3598fe79f014ed2
--replace-all <--replace-all>
2025-10-23 13:05:46 -0600

    Commiting Lab 7 material

--- app/src/androidTest/java/com/example/androiduitesting/ShowActivityTest.java
+++ app/src/androidTest/java/com/example/androiduitesting/ShowActivityTest.java
@@ -4,14 +4,17 @@ import static androidx.test.espresso.Espresso.onData;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasExtra;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
-
+import static androidx.test.espresso.intent.Intents.intended;
 import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.hamcrest.CoreMatchers.is;
 
 import androidx.test.espresso.action.ViewActions;
+import androidx.test.espresso.intent.Intents;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.LargeTest;
@@ -30,13 +33,19 @@ public class ShowActivityTest {
 
     @Test
     public void checkActivitySwitch(){
+        Intents.init();
         onView(withId(R.id.button_add)).perform(click());
         onView(withId(R.id.editText_name)).perform(ViewActions.typeText("Edmonton"));
         onView(withId(R.id.button_confirm)).perform(click());
 
         onData(is(instanceOf(String.class))).inAdapterView(withId(R.id.city_list)).atPosition(0).perform(click());
 
-        onView(withId(R.id.text_cityName)).check(matches(isDisplayed()));
+        intended(hasComponent(ShowActivity.class.getName()));
+        intended(hasExtra("city","Edmonton"));
+        Intents.release();
+
+
+
     }
     @Test
     public void checkCityCarryover(){
